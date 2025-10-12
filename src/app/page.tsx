759eb09a97507fe943e3660fdea3d69f6ee4f544
Maddox Chivam <emberamanufactura@gmail.com>
2025-10-12 18:57:53 +0000

    quita encabezado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Footer from "@/components/site/footer";
-import Header from "@/components/site/header";
 import Hero from "@/components/site/hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh">
-      <Header />
       <main className="flex-1">
         <Hero />
       </main>
