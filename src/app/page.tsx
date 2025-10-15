21dce43645292db34d6dfd69688bf7fa75ae6340
Maddox Chivam <emberamanufactura@gmail.com>
2025-10-15 17:44:13 +0000

    hijo de puta pedazo de idiota, te digo no ecabezado y me haces semejante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,6 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh">
       <main className="flex-1">
-        <div className="w-full flex justify-center py-8 bg-background">
-            <Image
-                src="/imagen02.jpeg"
-                alt="Imagen superior"
-                width={800}
-                height={450}
-                className="object-cover rounded-lg"
-            />
-        </div>
         <Hero />
       </main>
       <Footer />
