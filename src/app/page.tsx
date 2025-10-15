77cd5340a32862f43c1793e6f806e3888a51d07f
Maddox Chivam <emberamanufactura@gmail.com>
2025-10-15 17:42:03 +0000

    como quieres que no explote en ira

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex-1">
         <div className="w-full flex justify-center py-8 bg-background">
             <Image
-                src="/image02.jpeg"
+                src="/imagen02.jpeg"
                 alt="Imagen superior"
                 width={800}
                 height={450}
